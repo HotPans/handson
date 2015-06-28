39686964ff32024d286d502e13111b98670d6ea2
T.Kamiya <gatto3@gatto.jp>
2015-06-28 23:12:06 +0900

    new add message

--- src/hotpans/HelloWorld.java
+++ src/hotpans/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("makopi23");
         System.out.println("makopi23 GitHub Test");
+        System.out.println("kami256  GitHub Test");
     }
 
 }
